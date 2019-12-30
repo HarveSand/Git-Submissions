d4cff2efdec2fc02ad02d7e49f2e57ef5182e403
Harvey <Harvey@DESKTOP-289LH5P>
2019-12-30 14:10:48 +0800

    Multiplication Table

---
+++ GitSubmits/src/MultiTable/MultiplicationTable.java
@@ -0,0 +1,18 @@
+
+package MultiTable;
+
+
+public class MultiplicationTable {
+
+    
+    public static void main(String[] args) {
+        
+            for( int y=1; y<=10; y++){
+        for (int x=1; x<=10; x++){
+            System.out.print(x+" X "+y+" = "+x*y+"\t");
+        }System.out.println();
+    
+    }
+    
+}
+}
